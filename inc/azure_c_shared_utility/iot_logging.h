f3868fe04513efe87ef72680482f93b9473daebb
mamokarz <mamokarz@microsoft.com>
2016-06-08 18:43:05 -0700

    fixed logging to work with Arduino Huzzah, and create httpapi, and threadapi for Arduino.

--- inc/azure_c_shared_utility/iot_logging.h
+++ inc/azure_c_shared_utility/iot_logging.h
@@ -21,6 +21,25 @@ extern "C" {
 
 #define LOG_LINE 0x01
 
+/*
+ESP8266 has limited RAM so we force all logging strings to PROGMEM (flash)
+and we leverage os_printf rather than printf.
+
+Time inclusion in errors is possible if NTP was successful, and can be added
+in a later revision.
+*/
+
+#if defined(ARDUINO_ARCH_ESP8266)
+#include "esp8266/azcpgmspace.h"
+#define LogUsage(FORMAT, ...) { \
+        const char* __localFORMAT = PSTR(FORMAT); \
+        os_printf(__localFORMAT, ##__VA_ARGS__); \
+        os_printf("\r\n"); \
+}
+#define LogInfo LogUsage
+#define LogError LogUsage
+#else
+
 //Adding a do while(0) to force the user to add ; after LogInfo and LogError.
 #if defined _MSC_VER
 #define LogInfo(FORMAT, ...) do{(void)fprintf(stdout,"Info: " FORMAT "\r\n", __VA_ARGS__); }while(0)
@@ -34,10 +53,12 @@ extern "C" {
 #define LogError(FORMAT, ...) do{ time_t t = time(NULL); (void)fprintf(stderr,"Error: Time:%.24s File:%s Func:%s Line:%d " FORMAT "\r\n", ctime(&t), __FILE__, __func__, __LINE__, ##__VA_ARGS__); }while(0)
 #endif
 
+#endif /* ARDUINO_ARCH_ESP8266 */
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
-#endif
+#endif /* NO_LOGGING */
 
 #endif /* LOGGING_H */
